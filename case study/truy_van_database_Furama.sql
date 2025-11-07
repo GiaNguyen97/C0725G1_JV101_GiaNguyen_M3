01c0d3527a1bbb57f9eb490d35a318f3d91dfc5b
GiaNguyen97 <tranlegianguyen97dn@gmail.com>
2025-11-07 13:50:46 +0700

    thực hành và bài tập ss6

--- case study/truy_van_database_Furama.sql
+++ case study/truy_van_database_Furama.sql
@@ -11,11 +11,39 @@ and ((year(kh.ngay_sinh) >= year(curdate()) - 49 and month(kh.ngay_sinh) >= mont
 or year(kh.ngay_sinh) >= year(curdate()) - 50)
 and (kh.dia_chi like '%, Đà Nẵng' or kh.dia_chi like '%, Quảng Trị');
 
--- câu 4: Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+/* câu 4: Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần.
+Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”. */
+select kh.ma_khach_hang, kh.ho_ten, count(hd.ngay_lam_hop_dong) as so_lan_dat_phong
+from khach_hang as kh
+join hop_dong as hd on kh.ma_khach_hang = hd.ma_khach_hang
+join loai_khach as lk on kh.ma_loai_khach = lk.ma_loai_khach
+where  lk.ten_loai_khach = 'Diamond'
+group by kh.ma_khach_hang
+order by so_lan_dat_phong asc;
 
--- Câu 5: Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+/* Câu 5: Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
+(Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, 
+với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. 
+(những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra). */
+select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc,
+sum(dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as tong_tien
+from khach_hang as kh
+left join hop_dong as hd on kh.ma_khach_hang = hd.ma_khach_hang
+left join loai_khach as lk on kh.ma_loai_khach = lk.ma_loai_khach
+left join dich_vu as dv on hd.ma_dich_vu = dv.ma_dich_vu
+left join hop_dong_chi_tiet as hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+left join dich_vu_di_kem as dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by kh.ma_khach_hang,hd.ma_hop_dong; 
 
--- Câu 6: Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+/* Câu 6: Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3). */
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue,ldv.ten_loai_dich_vu,hd.ngay_lam_hop_dong
+from dich_vu as dv
+left join loai_dich_vu as ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+left join hop_dong as hd on dv.ma_dich_vu = hd.ma_dich_vu
+left join khach_hang as kh on hd.ma_khach_hang = kh.ma_khach_hang
+where month(hd.ngay_lam_hop_dong)
 
 -- Câu 7: Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
 
